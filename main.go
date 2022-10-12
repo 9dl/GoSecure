ebd254505802c4ecf84b368541cbf05a70393f99
9dl <80607793+9dl@users.noreply.github.com>
2022-10-12 02:03:50 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/process"
+)
+
+func main() {
+	for {
+		pids, _ := process.Pids()
+
+		for _, p := range pids {
+			pid, _ := process.NewProcess(p)
+			name, _ := pid.Name()
+
+			switch name {
+			case "x64dbg.exe":
+				fmt.Println("Killed:", name)
+				pid.Kill()
+			case "x32dbg.exe":
+				fmt.Println("Killed:", name)
+				pid.Kill()
+			}
+		}
+	}
+
+}
